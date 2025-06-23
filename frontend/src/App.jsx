020bb87ff33971d1dc1df1c216eadd9f8a96eefb
neutrino-ashutosh <neutrino.ashutosh@gmail.com>
2025-06-23 17:24:30 +0530

    added react querry to interact with api

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import usePing from './hooks/apis/queries/usePing'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const { isLoading, data } = usePing();
+
+  if (isLoading) {
+    return (
+      <>
+        Loading...
+      </>
+    )
+  }                                                                                                                                                                 
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  <>
+    Hello {data.message}
+  </>
   )
 }
 
